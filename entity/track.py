fec782b32f6f5c6d6a9e5b703b09bb8626534693
tboutroux <boutrouxtheo@gmail.com>
2025-11-17 17:38:02 +0100

    add entities

---
+++ entity/track.py
@@ -0,0 +1,31 @@
+from base import Base
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+
+
+class Track(Base):
+    """
+    Track entity representing a musical track.
+
+    Description:
+    This class defines the structure of the 'tracks' table in the database.
+    It includes attributes such as id, title, duration, popularity, and album_id.
+    """
+    __tablename__ = 'tracks'
+
+    id = Column(String, primary_key=True)
+    track_name = Column(String, unique=True, nullable=False)
+    album_id = Column(String, ForeignKey('albums.id'), nullable=False)
+    duration_ms = Column(Integer, nullable=True)  # duration of the track in milliseconds
+    main_artist_id = Column(String, ForeignKey('artists.id'), nullable=False)
+    popularity = Column(Integer, nullable=True)
+    track_cover_uri = Column(String, unique=True, nullable=False)
+
+    # Relationships
+    album = relationship('Album', back_populates='tracks')
+    main_artist = relationship('Artist', back_populates='tracks')
+    featuring_artists = relationship('Artist', secondary='feats', back_populates='featured_tracks')
+    histories = relationship('History', back_populates='track', cascade='all, delete-orphan')
+
+    def __repr__(self):
+        return f"<Track(id={self.id}, track_name='{self.track_name}', album_id={self.album_id})>"
---
+++ entity/user.py
@@ -0,0 +1,24 @@
+from base import Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+
+
+class User(Base):
+    """
+    User entity representing a user in the system.
+
+    Description:
+    This class defines the structure of the 'users' table in the database.
+    It includes attributes such as id, display name, and profile picture URI.
+    """
+    __tablename__ = 'users'
+
+    id = Column(String, primary_key=True)
+    display_name = Column(String, unique=True, nullable=False)
+    profile_picture_uri = Column(String, unique=True, nullable=False)
+
+    def __repr__(self):
+        return f"<User(id={self.id}, display_name='{self.display_name}', profile_picture_uri='{self.profile_picture_uri}')>"
+
+    # Relationships
+    histories = relationship('History', back_populates='user', cascade='all, delete-orphan')
